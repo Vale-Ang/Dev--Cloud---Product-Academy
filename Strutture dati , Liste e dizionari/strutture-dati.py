77c362b25f4c3a67c9f0da2dbddeccf298f6c82e
Vale <valeriaangioj@gmail.com>
2026-01-03 17:12:59 +0100

    lista con diverse variabili

--- Strutture dati , Liste e dizionari/strutture-dati.py
+++ Strutture dati , Liste e dizionari/strutture-dati.py
@@ -1,13 +1,27 @@
-stringhe: list[str] = ["pippo", "pluto", "paperino"]
-stringhe.append("topolino") 
-stringhe.append("minnie")
+stringhe: list[str | int] = ["Pippo", 1, "Pluto", "Paperino"]
+stringhe.append("Topolino") 
+stringhe.append("Minnie")
 # for x in stringhe:
 #     print(x)
 
 print(stringhe)
-# print(stringhe[1])
+# print(stringhe[1]) 
 deleted_values: list[str] = []
-deleted_value = stringhe.pop()
-deleted_values.append(deleted_value)
+
+value_to_check_and_delete: str = "aperino"
+
+is_value_in_the_list: bool = value_to_check_and_delete in stringhe
+print(is_value_in_the_list)
+
+if is_value_in_the_list:
+    index_value_to_delete = stringhe.index(value_to_check_and_delete)
+    print(index_value_to_delete)
+    deleted_value = stringhe.pop(index_value_to_delete)
+    deleted_values.append(deleted_value)
+else:
+    print(f" {value_to_check_and_delete} non esiste nella lista {stringhe}")
+
+print("*"*30)
+print(stringhe)
 print(deleted_values)
 
