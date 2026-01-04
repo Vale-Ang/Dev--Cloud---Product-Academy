beb13c3486fea983622ef8fa9a6962c85e518927
Vale <valeriaangioj@gmail.com>
2026-01-04 15:34:31 +0100

    Esercizi: slicing

---
+++ Strutture dati , Liste e dizionari/esercizi/slicing.py
@@ -0,0 +1,5 @@
+temperature : list [int] = [15, 18, 22, 25, 28, 30, 27, 24, 20]
+print(f" Prima temperatura: {temperature[0]}")
+print(f"Ultima temperatura: {temperature[len(temperature)-1]}")
+print(f"Temperature [2:5]: {temperature[2:5]}")
+print(f"Ogni due: {temperature[0::2]}")
