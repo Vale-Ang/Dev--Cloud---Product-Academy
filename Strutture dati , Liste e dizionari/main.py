871f4df460e8d1be3836fed438d160dd300101c0
Vale <valeriaangioj@gmail.com>
2026-01-05 18:25:51 +0100

    aggiunto liste e dizionari al quiz

--- Strutture dati , Liste e dizionari/main.py
+++ Strutture dati , Liste e dizionari/main.py
@@ -90,6 +90,13 @@ def estrai_risposte(content: str, index: int) -> str:
     return content[index+1:]
     # return answers
 
+def estrai_lista_domande(file_path: str) -> list[str]:
+    lista_domande: list[str] = []
+    with open(file_path,"r") as f:
+        for i in f:
+            lista_domande.append(i.strip())
+    return lista_domande
+
 def main():
     domande_list: list[str] = []
     qa: dict[str, str] = {
@@ -97,89 +104,46 @@ def main():
         "risposta": None
     }
 
-    with open("domande.txt","r") as f:
-        for i in f:
-            # print(i)
-            # print(i.strip())
-            domande_list.append(i.strip())
-        # print(f.read())
+    counter: int = 0
+
+    # with open("domande.txt","r") as f:
+    #     for i in f:
+    #         # print(i)
+    #         # print(i.strip())
+    #         domande_list.append(i.strip())
+    #     # print(f.read())
 
     # with open(domande_list[0],"r") as f:
     #     for i in f:
     #         print(i.strip())
 
-    content: str = leggi_file(f"domande_risposte/{domande_list[1]}")
-    # print(domande_list)
+    domande_list = estrai_lista_domande("domande.txt")
+    counter= len(domande_list)
+    # print(f"domande: {counter}")
 
-    index: int = estrai_index(content)
-    # domanda: str = estrai_domanda(content, index)
-    qa["domanda"] = estrai_domanda(content, index)
-    # risposta: str = estrai_risposte(content, index)
-    qa["risposta"] = estrai_risposte(content, index)
-    print(qa)
-    # print(domanda)
-    # print(risposta)
-'''
-    file_path: str = sys.argv[1]
-    # print(sys.argv[1])
-    content: str = leggi_file(file_path)
-    
-  
-
-    is_risposta_corretta: bool = False
-    while True:
-        mostra_domanda(domanda)
-    
+    for q in range(counter):
+        content: str = leggi_file(f"domande_risposte/{domande_list[q]}")
+        # print(domande_list)
+        index: int = estrai_index(content)
+        qa["domanda"] = estrai_domanda(content, index)
+        qa["risposta"] = estrai_risposte(content, index)
+        # input(qa["domanda"])
+        # print(qa)
+        mostra_domanda(qa["domanda"])
         risposta_da_validare: str = raccogli_risposta()
         risposta_validarta: bool = valida_scelta(risposta_da_validare)
         feedback: str = ""
 
         if risposta_validarta == True:
-            is_risposta_corretta = is_risposta_esatta(risposta_da_validare, risposta)
+            is_risposta_corretta: bool = is_risposta_esatta(risposta_da_validare, qa["risposta"])
             feedback = genera_feedback(is_risposta_corretta)
         else:
             feedback = "Inserisci solo la risposta tra le opzioni elencate"
-        
+
         mostra_feedback(feedback)
-        if is_risposta_corretta == True:
-            break
-'''
+        
+
+
 #Entry point del nostro programma
 main()
 
-"""
-mostra_menu() (senza return)
-- Non prende parametri
-- Stampa la domanda e le 4 opzioni
-- Non restituisce nulla
-
-raccogli_risposta() (con return)
-- Non prende parametri
-- Chiede l'input all'utente
-- Restituisce la scelta
-
-valida_scelta(scelta) (con return)
-- prende come parametro il valore scelto
-- Verifica se è A, B, C o D usando if
-- Restituisce True se valida, False altrimenti
-
-genera_feedback(scelta) (con return)
-- Prende come parametro la lettera che è stata scelta
-- Usa if/elif/else per determinare il messaggio
-- Restituisce la stringa con il feedback personalizzato
-
-mostra_feedback(messaggio) (senza return)
-- Prende come parametro una stringa
-- Stampa il feedback in modo formattato
-- Non restituisce nulla
-"""
-
-"""
-mostra_domanda()
-
-risposta_da_validare: str = raccogli_risposta()
-risposta_validata: bool = valida_scelta(risposta_da_validare)
-
-print(risposta_validata)
-
-"""
