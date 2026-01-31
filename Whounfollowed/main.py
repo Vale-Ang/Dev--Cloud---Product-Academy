ccb4405696f728386fefe293019f08d2c49d3e4a
Vale <valeriaangioj@gmail.com>
2026-01-31 20:32:11 +0100

    Whounfollowed

---
+++ Whounfollowed/main.py
@@ -0,0 +1,4 @@
+"""
+Voglio creare un programma che sia in grado di accedere alla mia lista di follower su Github e che riesca a tracciarli gio rno per giorno. 
+In questo modo posso capire chi ha smesso di seguirmi oppure chi mi segue.
+"""
